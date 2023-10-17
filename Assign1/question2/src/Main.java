60500fc186f7be85a85dee43fc40cb02901614a6
Sumant123 <sumant1.ca@gmail.com>
2023-10-16 23:13:57 -0400

    Add files via upload

---
+++ Assign1/question2/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+public class Main {
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] ={3,60,35,2,45,320,5};
+
+        System.out.println("Array Before Bubble Sort");
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        bubbleSort(arr);//sorting array elements using bubble sort
+
+        System.out.println("Array After Bubble Sort");
+        for(int i=0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        Random rd = new Random(); // creating Random object
+        int[] a = new int[5];
+        for (int i = 0; i < a.length; i++)
+        {
+            a[i] = rd.nextInt(); // storing random integers in an array
+            System.out.println(a[i]); // printing each array element
+        }
+
+    }
+}
